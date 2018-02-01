450c530c71c0f6bc2aaca1de294fd7ba7f0614a0
ranjithstreak <35728127+ranjithstreak@users.noreply.github.com>
2018-02-01 16:42:23 +0530

    Create ragul2.c

---
+++ ragul2.c
@@ -0,0 +1,14 @@
+int i,j,n,k,sum=0;
+printf("\n enter the integers values");
+scanf("%d%d",&n ,&k);
+for(j=1,j<n,j++)
+{
+printf("\n",j);
+}
+for(i=1,i<k,i++)
+{
+sum=sum+i;
+}
+printf("\nThe sum of integers is%d",sum);
+getch();
+}
