49d35db9afd4a859e85139d9695b45d150d1d714
ranjithstreak <35728127+ranjithstreak@users.noreply.github.com>
2018-02-06 16:57:36 +0530

    Create rko.c

---
+++ rko.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int i,a,b;
+    printf("enter the numbers a,b\n");
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<=b;i++)
+    if(i%2==1)
+    printf(" %d\n",i);
+}
